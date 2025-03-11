c62c96904f70bbc9ff29994fb592d58ca1f2211b
javadkavossi <jkavossi@gmail.com>
2025-03-11 22:18:05 +0330

    update

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,51 @@
-FROM heroiclabs/nakama:3.25.0
-
-WORKDIR /nakama/data/modules
-COPY backend.so .
-COPY local.yml /nakama/data/
-COPY *.json /nakama/data/modules
+# مرحله اول: بیلد برای معماری ARM
+FROM golang:1.17-alpine AS builder
 
 
-# GOARCH=arm64 GOOS=darwin go build -trimpath -buildmode=plugin -o backend.so
-# docker-compose up --build
+RUN apk add --no-cache build-base
 
 
-# go build --trimpath --buildmode=plugin -o backend.so  
+RUN apk add --no-cache \
+    build-base \
+    gcc \
+    binutils \
+    musl-dev 
+
+
+ENV GO111MODULE=on
+ENV CGO_ENABLED=1
+
+
+WORKDIR /nakama/data
+
+
+COPY go.mod go.sum ./
+
+
+RUN go mod tidy
+
+
+COPY . .
+
+
+RUN GOARCH=arm64 go build -buildmode=plugin -o /nakama/data/modules/backend.so main.go
+
+
+RUN chmod +x /nakama/data/modules/backend.so
+
+
+FROM --platform=linux/arm64 heroiclabs/nakama:3.22.0 AS runtime
+
+
+WORKDIR /nakama
+
+
+COPY --from=builder /nakama/data/modules/backend.so /nakama/data/modules/backend.so
+COPY --from=builder /nakama/data/local.yml /nakama/data/local.yml
+COPY --from=builder /nakama/data/modules/*.json /nakama/data/modules/
+
+
+RUN chmod +x /nakama/nakama
+
+
+CMD ["/nakama/nakama", "migrate", "up", "--database.address", "postgres:localdb@postgres:5432/nakama"]
--- main.go
+++ main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"time"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
-func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializers runtime.Initializer) error {
-    initStart := time.Now()
-    err := initializers.RegisterRpc("healthcheck", RpcHealthcheck)
-    if err != nil {
-        logger.Error("ثبت RPC healthcheck با مشکل مواجه شد: %s", err)
-        return err
-    }
-    logger.Info("ماژول با موفقیت در %s بارگذاری شد", time.Since(initStart).Milliseconds())
-    return nil
+type HealthcheckResponse struct {
+	Success bool `json:"success"`
 }
 
+func RpcHealthcheck(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	logger.Debug("Healthcheck RPC called ")
+	response := &HealthcheckResponse{
+		Success: true,
+	}
+	out, err := json.Marshal(response)
+	if err != nil {
+		logger.Error("Failed to marshal healthcheck response: %s", err)
+		return "", runtime.NewError("failed to marshal healthcheck response", 13)
+	}
+	return string(out), nil
+}
 
+func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializers runtime.Initializer) error {
+	initStart := time.Now()
+	err := initializers.RegisterRpc("healthcheck", RpcHealthcheck)
+	if err != nil {
+		logger.Error("ثبت RPC healthcheck با مشکل مواجه شد: %s", err)
+		return err
+	}
+	logger.Info("ماژول با موفقیت در %s بارگذاری شد", time.Since(initStart).Milliseconds())
+	return nil
+}
